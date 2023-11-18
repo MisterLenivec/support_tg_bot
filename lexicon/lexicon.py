4947d0758f344b98cbb62610c384561ed56fec09
MisterLenivec <wormsom@gmail.com>
2023-11-18 19:32:51 +0300

    Added bot configuration and Menu button settings

---
+++ lexicon/lexicon.py
@@ -0,0 +1,10 @@
+LEXICON_COMMANDS: dict[str, str] = {
+    '/menu': 'Главное меню',
+    '/info': 'Что умеет Емеля?',
+    '/advantage': 'Преимущества',
+    '/contacts': 'Контакты',
+    '/docs_install': 'Инструкция по установке',
+    '/home_assistant': 'Home Assistant',
+    '/operator': 'Связь с оператором',
+    '/send_feedback': 'Отправить форму обратной связи'
+}
